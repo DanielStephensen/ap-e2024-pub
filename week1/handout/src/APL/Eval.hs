99303c9f6b20f5314cba3917812e104ce43ed9ba
DanielStephensen <daniel@stephensen.net>
2024-09-05 15:16:10 +0200

    Exercises finished

--- week1/handout/src/APL/Eval.hs
+++ week1/handout/src/APL/Eval.hs
@@ -1,6 +1,111 @@
 module APL.Eval
   (
+    Val(..),
+    eval,
+    envEmpty
   )
 where
 
-import APL.AST ()
+import APL.AST (Exp (..), VName)
+import Data.Maybe (fromMaybe)
+
+type Env = [(VName, Val)]
+
+envEmpty :: Env
+envEmpty = []
+
+envExtend :: VName -> Val -> Env -> Env
+envExtend vName val env = (vName, val) : env
+
+-- | Look up a variable name in the provided environment.
+-- Returns Nothing if the variable is not in the environment.
+envLookup :: VName -> Env -> Maybe Val
+envLookup vName env = lookup vName env
+
+data Val
+  = ValInt Integer
+  | ValBool Bool
+  deriving (Eq, Show)
+
+type Error = String
+
+eval :: Env -> Exp -> Either Error Val
+eval env (CstInt x) = Right $ ValInt x
+eval env (Add x y) = 
+  case (eval env x, eval env y) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (Right (ValInt x), Right (ValInt y)) -> Right $ ValInt $ x + y
+    (_, Right y) -> Left "Error: Cannot add x and y together, since x doesn't have type Val"
+    (Right x, _) -> Left "Error: Cannot add x and y together, since y doesn't have type Val"
+    _ -> Left "Error: Cannot add x and y together, since neither value has type Val"
+
+eval env (Sub x y) = 
+  case (eval env x, eval env y) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (Right (ValInt x), Right (ValInt y)) -> Right $ ValInt $ x - y
+    (_, Right y) -> Left "Error: Cannot sub x and y together, since x doesn't have type Val"
+    (Right x, _) -> Left "Error: Cannot sub x and y together, since y doesn't have type Val"
+    _ -> Left "Error: Cannot sub x and y together, since neither value has type Val"
+
+eval env (Mul x y) = 
+  case (eval env x, eval env y) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (Right (ValInt x), Right (ValInt y)) -> Right $ ValInt $ x * y
+    (_, Right y) -> Left "Error: Cannot mul x and y together, since x doesn't have type Val"
+    (Right x, _) -> Left "Error: Cannot mul x and y together, since y doesn't have type Val"
+    _ -> Left "Error: Cannot mul x and y together, since neither value has type Val"
+
+eval env (Div x y) = 
+  case (eval env x, eval env y) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (Right (ValInt x), Right (ValInt 0)) -> Left "Error: Division by zero is not supported"
+    (Right (ValInt x), Right (ValInt y)) -> Right $ ValInt $ x `div` y
+    (_, Right y) -> Left "Error: Cannot div x and y together, since x doesn't have type Val"
+    (Right x, _) -> Left "Error: Cannot div x and y together, since y doesn't have type Val"
+    _ -> Left "Error: Cannot div x and y together, since neither value has type Val"
+
+eval env (Pow x y) = 
+  case (eval env x, eval env y) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (Right (ValInt x), Right (ValInt y)) 
+      | y < 0 -> Left "Error: Negative exponents are not supported"
+      | otherwise -> Right $ ValInt $ x ^ y
+    (_, Right y) -> Left "Error: Cannot pow x and y together, since x doesn't have type Val"
+    (Right x, _) -> Left "Error: Cannot pow x and y together, since y doesn't have type Val"
+    _ -> Left "Error: Cannot pow x and y together, since neither value has type Val"
+
+eval env (CstBool x) = Right $ ValBool x
+
+eval env (Eql x y) = 
+  case (eval env x, eval env y) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (Right (ValInt x), Right (ValInt y)) -> Right (ValBool (x == y)) 
+    (Right (ValBool x), Right (ValBool y)) -> Right (ValBool (x == y)) 
+    (Right _, Right _) -> Left "Error: Cannot compare x and y since they have different types"
+    _ -> Left "Error: Cannot pow x and y together, since neither value has type Val"
+
+eval env (If cond x y) = 
+  case (eval env cond) of
+    Left err -> Left err
+    Right (ValBool True) -> eval env x
+    Right (ValBool False) -> eval env y
+    Right _ -> Left "Error: Condition is not a boolean"
+
+eval env (Var x) = 
+  case (envLookup x env) of
+    Nothing -> Left "Error: Variable not found in environment"
+    Just v -> Right v
+
+eval env (Let vName expr1 expr2) = 
+  case (eval env expr1) of
+    Left err -> Left err
+    Right v -> eval (envExtend vName v env) expr2
+
+  
+
--- week1/handout/src/APL/Eval_Tests.hs
+++ week1/handout/src/APL/Eval_Tests.hs
@@ -1,12 +1,76 @@
 module APL.Eval_Tests (tests) where
 
-import APL.AST ()
-import APL.Eval ()
+
+import APL.AST (Exp (..))
+import APL.Eval (Val (..), eval, envEmpty)
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit ()
+import Test.Tasty.HUnit (testCase, assertBool, (@?=))
 
 tests :: TestTree
 tests =
   testGroup
     "Evaluation"
-    []
+    [testCstInt0, testAdd0, testSub0, testMul0, testDiv0, testDiv1, testPow0, testPow1, testEql0, testEql1, testEql2, testEql3, testEql4, testEql4, testIf0, testIf1, testIf2, testIf3, testIf4, testLet0, testLet1]
+
+testCstInt0 :: TestTree
+testCstInt0 = testCase "should work" $ eval envEmpty (CstInt 5) @?= Right (ValInt 5)
+
+testAdd0 :: TestTree
+testAdd0 = testCase "should work" $ eval envEmpty (Add (CstInt 5) (CstInt 4)) @?= Right (ValInt 9)
+
+testSub0 :: TestTree
+testSub0 = testCase "should work" $ eval envEmpty (Sub (CstInt 5) (CstInt 4)) @?= Right (ValInt 1)
+
+testMul0 :: TestTree
+testMul0 = testCase "should work" $ eval envEmpty (Mul (CstInt 5) (CstInt 4)) @?= Right (ValInt 20)
+
+testDiv0 :: TestTree
+testDiv0 = testCase "should work" $ eval envEmpty (Div (CstInt 8) (CstInt 4)) @?= Right (ValInt 2)
+
+testDiv1 :: TestTree
+testDiv1 = testCase "should work" $ eval envEmpty (Div (CstInt 8) (CstInt 0)) @?= Left "Error: Division by zero is not supported"
+
+testPow0 :: TestTree
+testPow0 = testCase "should work" $ eval envEmpty (Pow (CstInt 2) (CstInt 4)) @?= Right (ValInt 16)
+
+testPow1 :: TestTree
+testPow1 = testCase "should work" $ eval envEmpty (Pow (CstInt 2) (CstInt (-4))) @?= Left "Error: Negative exponents are not supported"
+
+testEql0 :: TestTree
+testEql0 = testCase "should work" $ eval envEmpty (Eql (CstInt 2) (CstInt 1)) @?= Right (ValBool False)
+
+testEql1 :: TestTree
+testEql1 = testCase "should work" $ eval envEmpty (Eql (CstInt 2) (CstInt 2)) @?= Right (ValBool True)
+
+testEql2 :: TestTree
+testEql2 = testCase "should work" $ eval envEmpty (Eql (CstBool True) (CstBool False)) @?= Right (ValBool False)
+
+testEql3 :: TestTree
+testEql3 = testCase "should work" $ eval envEmpty (Eql (CstBool True) (CstBool True)) @?= Right (ValBool True)
+
+testEql4 :: TestTree
+testEql4 = testCase "should work" $ eval envEmpty (Eql (CstBool False) (CstBool False)) @?= Right (ValBool True)
+
+testEql5 :: TestTree
+testEql5 = testCase "should work" $ eval envEmpty (Eql (CstBool False) (CstInt 1)) @?= Left "Error: Cannot compare x and y since they have different types"
+
+testIf0 :: TestTree
+testIf0 = testCase "should work" $ eval envEmpty (If (CstBool True) (CstInt 2) (CstInt 1)) @?= Right (ValInt 2)
+
+testIf1 :: TestTree
+testIf1 = testCase "should work" $ eval envEmpty (If (CstBool False) (CstInt 2) (CstInt 1)) @?= Right (ValInt 1)
+
+testIf2 :: TestTree
+testIf2 = testCase "should work" $ eval envEmpty (If (CstBool True) (Eql (CstBool False) (CstBool False)) (CstInt 1)) @?= Right (ValBool True)
+
+testIf3 :: TestTree
+testIf3 = testCase "should work" $ eval envEmpty (If (CstBool True) (Eql (CstBool False) (CstInt 1)) (CstInt 1)) @?= Left "Error: Cannot compare x and y since they have different types"
+
+testIf4 :: TestTree
+testIf4 = testCase "should work" $ eval envEmpty (If (CstInt 1) (CstInt 2) (CstInt 1)) @?= Left "Error: Condition is not a boolean"
+
+testLet0 :: TestTree
+testLet0 = testCase "should work" $ eval envEmpty (Let "x" (CstInt 3) (Add (Var "x") (Var "x"))) @?= Right (ValInt 6)
+
+testLet1 :: TestTree
+testLet1 = testCase "should work" $ eval envEmpty (Let "x" (CstInt 3) (Add (Var "x") (Var "y"))) @?= Left "Error: Variable not found in environment"
